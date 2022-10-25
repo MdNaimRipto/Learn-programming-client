71b25681c5b5dfc52f2504f0f4513a2d548577d6
MdNaimRipto <naimurtsc567@gmail.com>
2022-10-25 11:33:31 +0600

    Tooltip added

--- src/Pages/Shared/Nav/Nav.js
+++ src/Pages/Shared/Nav/Nav.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import logo from "../../../Assets/logo.png"
 import DarkModeToggle from "react-dark-mode-toggle";
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Dropdown, Navbar, Tooltip } from "flowbite-react";
 
 const Nav = () => {
     const [isDarkMode, setIsDarkMode] = useState(() => false);
@@ -10,48 +10,56 @@ const Nav = () => {
     return (
 
         <Navbar
-            className="container mx-auto"
+            className="container mx-auto my-2"
             fluid={true}
             rounded={true}
         >
             <Navbar.Brand href="https://flowbite.com/">
                 <img
-                    src="https://flowbite.com/docs/images/logo.svg"
+                    src={logo}
                     className="mr-3 h-6 sm:h-9"
                     alt="Flowbite Logo"
                 />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    Flowbite
+                    Learn Programing
                 </span>
             </Navbar.Brand>
             <div className="flex justify-between md:order-2 w-1/4 md:w-12">
-                <Dropdown
-                    arrowIcon={false}
-                    inline={true}
-                    label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                <Tooltip
+                    content="User Name"
+                    placement="bottom"
                 >
-                    <Dropdown.Header>
-                        <span className="block text-sm">
-                            Bonnie Green
-                        </span>
-                        <span className="block truncate text-sm font-medium">
-                            name@flowbite.com
-                        </span>
-                    </Dropdown.Header>
-                    <Dropdown.Item>
-                        Dashboard
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                        Settings
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                        Earnings
-                    </Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item>
-                        Sign out
-                    </Dropdown.Item>
-                </Dropdown>
+                    <Dropdown
+                        arrowIcon={false}
+                        inline={true}
+                        label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                    >
+
+                        <Dropdown.Header>
+                            <span className="block text-sm">
+                                Bonnie Green
+                            </span>
+                            <span className="block truncate text-sm font-medium">
+                                name@flowbite.com
+                            </span>
+                        </Dropdown.Header>
+
+
+                        <Dropdown.Item>
+                            Dashboard
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Settings
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Earnings
+                        </Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item>
+                            Sign out
+                        </Dropdown.Item>
+                    </Dropdown>
+                </Tooltip>
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
@@ -80,7 +88,7 @@ const Nav = () => {
                     />
                 </li>
             </Navbar.Collapse>
-        </Navbar>
+        </Navbar >
 
     );
 };
