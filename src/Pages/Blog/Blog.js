57c0da0aba7211bfee6410ad835faf328442ace2
MdNaimRipto <naimurtsc567@gmail.com>
2022-10-27 08:41:58 +0600

    blog section completed

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,7 +1,7 @@
 import { Card } from 'flowbite-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import faqImage from "../../Assets/faq.jpg"
+import blogImage from "../../Assets/blog.jpg"
 
 const Blog = () => {
     return (
@@ -14,18 +14,20 @@ const Blog = () => {
                             <p>We tried to write some blogs about programming. Hope it will helpful. Happy Reading!</p>
                         </div>
                         <div>
-                            <img src={faqImage} alt="" />
+                            <img src={blogImage} alt="" />
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='container mx-auto'>
+            <div className='container mx-auto mt-7'>
                 <Card className='mb-8 bg-[#fff1de9c]'>
                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         what is cors?
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        CORS is the mechanism that provides the ability to alter the behavior of this policy, enabling you to do things like hosting static content and the backend API. This kind of request would be called a Cross-Origin request, as a resource from one subdomain is requesting a resource from another subdomain.
+
+                        This is all controlled through preflight requests that exchange a set of HTTP request headers and corresponding response headers collectively referred to as "CORS Headers", each of these headers modifies a different element of the Same-Origin policy to loosen the limitations it imposes.
                     </p>
                 </Card>
                 <Card className='mb-8 bg-[#fff1de9c]'>
@@ -33,7 +35,7 @@ const Blog = () => {
                         Why are you using firebase? What other options do you have to implement authentication?
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        For user authentication and hosting I am using firebase. For implementing firebase authentication We need an auth, some authenticate functions and Firebase config file. And also a method to hide the firebase config file.
                     </p>
                 </Card>
                 <Card className='mb-8 bg-[#fff1de9c]'>
@@ -41,7 +43,7 @@ const Blog = () => {
                         How does the private route work?
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        PrivateRoute component is the blueprint for all private routes in the application. If the user is logged in, this route will take the user where he want's to go. Or if the user not logged in, then this route will take the user to the log in or signup page.
                     </p>
                 </Card>
                 <Card className='mb-12 bg-[#fff1de9c]'>
@@ -49,7 +51,7 @@ const Blog = () => {
                         What is Node? How does Node work?
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        Node.js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node.js wastes no time or resources on waiting for I/O requests to return. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
                     </p>
                 </Card>
             </div>
